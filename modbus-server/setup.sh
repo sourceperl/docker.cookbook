a5ab5f5a55164b8f8b0e572c346b0404eb28188c
sourceperl <loic.celine@free.fr>
2021-02-04 22:33:43 +0100

    fix modbus-server

--- modbus-server/setup.sh
+++ modbus-server/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t mbus-srv .
-docker run -d --restart always -p 502:5020 -t mbus-srv
+docker build -t mbus-srv-img .
+docker run -d --restart always -p 502:5020 --name=mbus-srv -t mbus-srv-img
