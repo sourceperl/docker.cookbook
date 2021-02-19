76e7cc118dc073ca203d3245f0c45b5830333aee
sourceperl <loic.celine@free.fr>
2021-02-19 19:23:34 +0100

    add mqtt-srv

---
+++ mqtt-srv/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CONTAINER="mqtt-srv"
+IMAGE="eclipse-mosquitto:1.6.13"
+NETWORK="mqtt-net"
+# PUB_PORT="-p 1883:1883"
+
+docker network create ${NETWORK}
+
+docker rm -f ${CONTAINER}
+
+docker run -d --name ${CONTAINER} \
+           --restart unless-stopped \
+           --network ${NETWORK} \
+           ${PUB_PORT} \
+           ${IMAGE}
