62dd732f7cc3885dff213acb9b81bc51df4d3a0c
sourceperl <loic.celine@free.fr>
2021-03-15 11:18:47 +0100

    update for mosquitto 2

--- mqtt-srv/setup.sh
+++ mqtt-srv/setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 CONTAINER="mqtt-srv"
-IMAGE="eclipse-mosquitto:1.6.13"
+IMAGE="eclipse-mosquitto:2.0.9"
 NETWORK="mqtt-net"
 # PUB_PORT="-p 1883:1883"
+CMD="mosquitto -c /mosquitto-no-auth.conf"
 
 
 docker network create ${NETWORK}
@@ -14,4 +15,5 @@ docker run -d --name ${CONTAINER} \
            --restart unless-stopped \
            --network ${NETWORK} \
            ${PUB_PORT} \
-           ${IMAGE}
+           ${IMAGE} \
+           ${CMD}
