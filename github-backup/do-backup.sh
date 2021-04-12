d7d4555e6d7cd155ec7242ce4a4458f6bb81ac2b
sourceperl <loic.celine@free.fr>
2021-04-12 23:53:02 +0200

    improve schedule

--- github-backup/do-backup.sh
+++ github-backup/do-backup.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 
-echo "$(date '+%Y-%m-%dT%H:%M:%S.000'): Start backup scheduler"
+log_msg () {
+    echo "$(date '+%Y-%m-%dT%H:%M:%S')    : $1"
+}
+
+log_msg "Start backup tool"
 
 while :; do
+    sleep 1m
+    log_msg "Wait next run schedule at 05:10"
+    while [ $(date '+%H:%M') != '05:10' ]; do sleep 15s; done
+
     DATE_DIR=$(date +%Y%m%d-%H%M%S)
 
     for user in $(echo $GITHUB_USER | tr "," "\n"); do
-        echo "$(date '+%Y-%m-%dT%H:%M:%S.000'): Execute backup for ${user}, ${DATE_DIR}"
+        log_msg "Execute backup for ${user}, ${DATE_DIR}"
         github-backup ${user} --token=$TOKEN --all --output-directory=/data/${DATE_DIR}/${user} --private --gists
     done
 
-    echo "$(date '+%Y-%m-%dT%H:%M:%S.000'): Do cleanup (keep last ${MAX_BACKUPS} backups)"
+    log_msg "Do cleanup (keep last ${MAX_BACKUPS} backups)"
     ls -d1 /data/* | head -n -${MAX_BACKUPS} | xargs rm -rf
-
-    echo "$(date '+%Y-%m-%dT%H:%M:%S.000'): Go to sleep"
-    sleep 1d
 done
